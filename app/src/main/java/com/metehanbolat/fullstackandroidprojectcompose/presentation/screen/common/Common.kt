47db4bf0d90fbd2a1d1b274083b2d1f7dfe60bca
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-02 18:10:11 +0300

    StartActivityForResult was created.

--- app/src/main/java/com/metehanbolat/fullstackandroidprojectcompose/presentation/screen/common/Common.kt
+++ app/src/main/java/com/metehanbolat/fullstackandroidprojectcompose/presentation/screen/common/Common.kt
@@ -2,10 +2,65 @@ package com.metehanbolat.fullstackandroidprojectcompose.presentation.screen.comm
 
 import android.app.Activity
 import android.util.Log
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.metehanbolat.fullstackandroidprojectcompose.util.Constants.CLIENT_ID
+
+@Composable
+fun StartActivityForResult(
+    key: Any,
+    onResultReceived: (String) -> Unit,
+    onDialogDismissed: () -> Unit,
+    launcher: (ManagedActivityResultLauncher<IntentSenderRequest, ActivityResult>) -> Unit
+) {
+    val activity = LocalContext.current as Activity
+    val activityLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        try {
+            if (result.resultCode == Activity.RESULT_OK) {
+                val oneTapClient = Identity.getSignInClient(activity)
+                val credentials = oneTapClient.getSignInCredentialFromIntent(result.data)
+                val tokenId = credentials.googleIdToken
+                if (tokenId != null) {
+                    onResultReceived(tokenId)
+                }
+            }else {
+                Log.d("StartActivityForResult", "BLACK SCRIM CLICKED, DIALOG CLICKED.")
+                onDialogDismissed()
+            }
+        } catch (e: ApiException) {
+            when(e.statusCode){
+                CommonStatusCodes.CANCELED -> {
+                    Log.d("StartActivityForResult", "ONE-TAP DIALOG CANCELED.")
+                    onDialogDismissed()
+                }
+                CommonStatusCodes.NETWORK_ERROR -> {
+                    Log.d("StartActivityForResult", "ONE-TAP NETWORK ERROR.")
+                    onDialogDismissed()
+                }
+                else -> {
+                    Log.d("StartActivityForResult", "${e.message}")
+                    onDialogDismissed()
+                }
+            }
+        }
+    }
+    
+    LaunchedEffect(key1 = key) {
+        launcher(activityLauncher)
+    }
+}
 
 fun signIn(
     activity: Activity,
