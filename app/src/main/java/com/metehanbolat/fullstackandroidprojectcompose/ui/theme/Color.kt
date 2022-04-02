3d88cf973516e2e2af323da9e55b8c18b7f821eb
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-02 14:37:03 +0300

    Google Button component was created.

--- app/src/main/java/com/metehanbolat/fullstackandroidprojectcompose/ui/theme/Color.kt
+++ app/src/main/java/com/metehanbolat/fullstackandroidprojectcompose/ui/theme/Color.kt
@@ -1,8 +1,22 @@
 package com.metehanbolat.fullstackandroidprojectcompose.ui.theme
 
+import androidx.compose.material.Colors
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Gray200 = Color(0xFF819ca9)
+val Gray500 = Color(0xFF546e7a)
+val Gray700 = Color(0xFF29434e)
+val ErrorRed = Color(0xFFFF6C60)
+val InfoGreen = Color(0xFF00C096)
+val LoadingBlue = Color(0xFF1A73E8)
+
+val Colors.topAppBarContentColor: Color
+    get() = if (isLight) Color.White else Color.LightGray
+
+val Colors.topAppBarBackgroundColor: Color
+    get() = if (isLight) Gray500 else Color.Black
