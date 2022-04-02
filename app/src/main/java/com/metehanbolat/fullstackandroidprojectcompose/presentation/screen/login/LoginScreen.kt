06cfe5823c2867aee0c57a0f8ad195e8e04b44dd
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-02 17:33:53 +0300

    LoginViewModel was created.

--- app/src/main/java/com/metehanbolat/fullstackandroidprojectcompose/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/metehanbolat/fullstackandroidprojectcompose/presentation/screen/login/LoginScreen.kt
@@ -2,17 +2,29 @@ package com.metehanbolat.fullstackandroidprojectcompose.presentation.screen.logi
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.metehanbolat.fullstackandroidprojectcompose.domain.model.MessageBarState
 
 @Composable
-fun LoginScreen(navController: NavHostController) {
+fun LoginScreen(
+    navController: NavHostController,
+    loginViewModel: LoginViewModel = hiltViewModel()
+) {
+    val signedInState by loginViewModel.signedState
+    val messageBarState by loginViewModel.messageBarState
+
     Scaffold(
         topBar = { LoginTopBar() },
         content = {
-            LoginContent(signedInState = true, messageBarState = MessageBarState()) {
-
-            }
+            LoginContent(
+                signedInState = signedInState,
+                messageBarState = messageBarState,
+                onButtonClicked = {
+                    loginViewModel.saveSignedInState(true)
+                }
+            )
         }
     )
 }
---
+++ app/src/main/java/com/metehanbolat/fullstackandroidprojectcompose/presentation/screen/login/LoginViewModel.kt
@@ -0,0 +1,41 @@
+package com.metehanbolat.fullstackandroidprojectcompose.presentation.screen.login
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.metehanbolat.fullstackandroidprojectcompose.domain.model.MessageBarState
+import com.metehanbolat.fullstackandroidprojectcompose.domain.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val repository: Repository
+): ViewModel() {
+
+    private val _signedInState: MutableState<Boolean> = mutableStateOf(false)
+    val signedState: State<Boolean> = _signedInState
+
+    private val _messageBarState: MutableState<MessageBarState> = mutableStateOf(MessageBarState())
+    val messageBarState: State<MessageBarState> = _messageBarState
+
+    init {
+        viewModelScope.launch {
+            repository.readSignedInState().collect { completed ->
+                _signedInState.value = completed
+            }
+        }
+    }
+
+    fun saveSignedInState(signedIn: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.saveSignedInState(signedIn = signedIn)
+        }
+    }
+
+}
